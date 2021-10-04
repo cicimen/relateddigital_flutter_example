7444192576570b567b9f612d42e69badd394012c
cicimen <egemengulkilik@gmail.com>
2021-10-04 12:59:37 +0300

    ui add for example project

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,128 @@
+import 'dart:async';
+
+import 'package:relateddigital_flutter/request_models.dart';
+import 'package:relateddigital_flutter_example/constants.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:relateddigital_flutter/relateddigital_flutter.dart';
+import 'package:relateddigital_flutter/response_models.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+  final RelateddigitalFlutter relatedDigitalPlugin = RelateddigitalFlutter();
+  String token = '-';
+  String email = 'egemen@visilabs.com';
+  final bool emailPermission = true;
+  @override
+  void initState() {
+    super.initState();
+    initLib();
+  }
+  Future<void> initLib() async {
+    var initRequest = RDInitRequestModel(
+      appAlias: Constants.APP_ALIAS,
+      huaweiAppAlias: Constants.HUAWEI_APP_ALIAS, // Android only
+      androidPushIntent: Constants.ANDROID_PUSH_INTENT, // Android only
+      organizationId: Constants.ORGANIZATION_ID,
+      siteId: Constants.SITE_ID,
+      dataSource: Constants.DATA_SOURCE,
+      maxGeofenceCount: Constants.MAX_GEOFENCE_COUNT,  // IOS only
+      geofenceEnabled: Constants.GEOFENCE_ENABLED,
+      inAppNotificationsEnabled: Constants.IN_APP_NOTIFICATIONS_ENABLED, // IOS only
+      logEnabled: Constants.LOG_ENABLED,
+    );
+    await relatedDigitalPlugin.init(initRequest, _readNotificationCallback);
+  }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  void _getTokenCallback(RDTokenResponseModel result) {
+    if(result != null && result.deviceToken != null && result.deviceToken.isNotEmpty) {
+      print('Token ' + result.deviceToken);
+      setState(() {
+        token = result.deviceToken;
+      });
+    }
+    else {
+      setState(() {
+        token = 'Token not retrieved';
+      });
+    }
+  }
 
-  // This widget is the root of your application.
+  void _readNotificationCallback(dynamic result) {
+    print('_readNotificationCallback');
+    print(result);
+  }
+  Future<void> requestPermission() async {
+    await relatedDigitalPlugin.requestPermission(_getTokenCallback);
+  }
+  Future<void> setEmail() async {
+    await relatedDigitalPlugin.setEmail(email, emailPermission);
+  }
+  Future<void> registerEmail() async {
+    bool permission = true;
+    bool isCommercial = false;
+    await relatedDigitalPlugin.registerEmail(email, permission: permission, isCommercial: isCommercial);
+    await relatedDigitalPlugin.setEmail('egemen@visilabs.com', true);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Text('Token: $token\n'),
+                ElevatedButton(
+                    onPressed: () {
+                      requestPermission();
+                    },
+                    child: Text('Get Token')
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            email = value;
+                          });
+                        },
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            setEmail();
+                          },
+                          child: Text('Set Email')
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            registerEmail();
+                          },
+                          child: Text('Register Email')
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
